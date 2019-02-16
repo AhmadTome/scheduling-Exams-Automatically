6f5d6e44d282696c50fd2ac43e57c605eebd9b02
AhmadTome <ahmad-400s@windowslive.com>
2019-02-16 12:13:19 -0800

    configre DB

---
+++ database/migrations/2019_02_16_160607_create_appointment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('session');// first session (8 - 10)
+            $table->string('start_AT');//8
+            $table->string('end_AT');//10
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointment');
+    }
+}
